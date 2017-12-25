3c8eb9cf0e951064130e3667b3ed30401b738981
subasasoccer <subasasoccer@gmail.com>
2017-12-26 01:34:03 +0700

    test fist git

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "extends": ["airbnb-base", "plugin:flowtype/recommended"],
+  "settings": {
+    "import/resolver": {
+      "node": {
+          "moduleDirectory": ["node_modules", "src/"]
+      }
+    }
+  }
+  "rules": {
+    "import/prefer-default-export": 0,
+    "no-console": 0,
+    "consistent-return": 0
+  }
+};
